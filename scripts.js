9e30957e2e4ca1c94726afb7aafe116acb11917e
Christian Workmaster <christianworkmaster@ChristianWorkmaster-Atl.local>
2019-02-04 13:25:43 -0500

    Have the result of the api call being published to the web page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 const app = document.getElementById('root');
 
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 var requestType = 'GET'
 var url = 'https://ghibliapi.herokuapp.com/films'
 
@@ -12,8 +21,24 @@ request.onload = function() {
 
     if (request.status >= 200 && request.status < 400) {
         data.forEach(movie => {
-            console.log(movie.title);
-          });
+            // Create a div with a card class
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            // Create an h1 and set the text context to the films title
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+
+            // Create a p and set the text context to the movie's description
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...`;
+
+            container.appendChild(card);
+
+            card.appendChild(h1);
+            card.appendChild(p);
+        });
     } else {
         console.log('error');
     }
