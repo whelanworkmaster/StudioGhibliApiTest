d5f73a72f306551a4e90fe59edf43e169ce582dc
Christian Workmaster <christianworkmaster@ChristianWorkmaster-Atl.local>
2019-02-18 14:24:05 -0500

    Added pagination to main screen, goes based on numDisplay variable

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const app = document.getElementById('root');
+const paginator = document.getElementById('paginator');
 
 const logo = document.createElement('img');
 logo.src = 'logo.png';
@@ -16,33 +17,57 @@ var request = new XMLHttpRequest();
 
 request.open(requestType, url, true);
 
+let data;
+let index=0;
+let numDisplay=4;
+
 request.onload = function() {
-    var data = JSON.parse(this.response)
-
+    data = JSON.parse(this.response)
     if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
-            // Create a div with a card class
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
-
-            // Create an h1 and set the text context to the films title
-            const h1 = document.createElement('h1');
-            h1.textContent = movie.title;
-
-            // Create a p and set the text context to the movie's description
-            const p = document.createElement('p');
-            movie.description = movie.description.substring(0, 300);
-            p.textContent = `${movie.description}...`;
-
-            container.appendChild(card);
-
-            card.appendChild(h1);
-            card.appendChild(p);
-        });
+        displayTable(data, index);
     } else {
         console.log('error');
     }
 }
 
+const displayTable = function(data, start) {
+    for(let i=start; i<start+numDisplay; i++) {
+        if(i === data.length) {
+            window.removeEventListener('scroll', pagination);
+        }
+        let movie = data[i];
+        // Create a div with a card class
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        // Create an h1 and set the text context to the films title
+        const h1 = document.createElement('h1');
+        h1.textContent = movie.title;
+
+        // Create a p and set the text context to the movie's description
+        const p = document.createElement('p');
+        movie.description = movie.description.substring(0, 300);
+        p.textContent = `${movie.description}...`;
+
+        container.appendChild(card);
+
+        card.appendChild(h1);
+        card.appendChild(p);
+        
+    };  
+    index = index+numDisplay;
+}
+
 request.send();
+
+const pagination = function() {
+    var rect = paginator.getBoundingClientRect();
+    if(rect.bottom < 0 || rect.right < 0 || rect.left > window.innerWidth || rect.top > window.innerHeight) {
+        console.log("index = " + index);
+        displayTable(data, index);
+    }
+}
+
+window.addEventListener('scroll', pagination);
+
 
